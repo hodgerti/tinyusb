a41c52fdca300357dc62b9d7e46d94ccae601df3
Anson Davis <orangehead321@gmail.com>
2019-01-31 11:14:43 -0800

    Testing adding comments on makefiles

--- hw/bsp/metro_m0_express/board.mk
+++ hw/bsp/metro_m0_express/board.mk
@@ -1,26 +1,26 @@
-CFLAGS = \
-	-DCFG_TUSB_MCU=OPT_MCU_SAMD21 \
+CFLAGS = \ #names of variables that are set to specify addition switches passed to compiler
+	-DCFG_TUSB_MCU=OPT_MCU_SAMD21 \ #tells TinyUSB what platform is being built (SAMD21)
 	-DCONF_DFLL_OVERWRITE_CALIBRATION=0 \
 	-D__SAMD21G18A__ \
-	-mthumb \
-	-mabi=aapcs-linux \
-	-mcpu=cortex-m0plus \
-	-msoft-float \
-	-mfloat-abi=soft
+	-mthumb \ #requests that the compiler targets the T32 instruction set (assembly files)
+	-mabi=aapcs-linux \ #designates Application Binary Interface (ABI) between the "Procedure Call for the ARM Architecture" (aapcs) and Linux. The ABI defines how data structures and computational routines are access in machine language, and here it helps Linux talk with the ARM architecture. 
+	-mcpu=cortex-m0plus \ #specifies name of target ARM processor (m0 in this case)
+	-msoft-float \ #disables generation of floating point instructions.
+	-mfloat-abi=soft #specifies the floating point ABI to use. Soft causes GCC to generate output containing library calls for floating-point operations. I think these things used together make it so that the hardware isn't responsible for floating point operations, and it is pushed off into software to eventually be done by other hardware. 
 
 # All source paths should be relative to the top level.
-LD_FILE = hw/bsp/metro_m0_express/samd21g18a_flash.ld
+LD_FILE = hw/bsp/metro_m0_express/samd21g18a_flash.ld #location to link-editor support library
 
-LD_FLAGS += -mthumb -mcpu=cortex-m0plus
+LD_FLAGS += -mthumb -mcpu=cortex-m0plus #same flags as above
 
-SRC_C += \
-	hw/mcu/microchip/samd/asf4/samd21/gcc/gcc/startup_samd21.c \
-	hw/mcu/microchip/samd/asf4/samd21/gcc/system_samd21.c \
-	hw/mcu/microchip/samd/asf4/samd21/hpl/gclk/hpl_gclk.c \
-	hw/mcu/microchip/samd/asf4/samd21/hpl/pm/hpl_pm.c \
-	hw/mcu/microchip/samd/asf4/samd21/hpl/sysctrl/hpl_sysctrl.c
+SRC_C += \ #source code c files
+	hw/mcu/microchip/samd/asf4/samd21/gcc/gcc/startup_samd21.c \ #includes startup handlers and definitions
+	hw/mcu/microchip/samd/asf4/samd21/gcc/system_samd21.c \ #sets and starts clock
+	hw/mcu/microchip/samd/asf4/samd21/hpl/gclk/hpl_gclk.c \ #not sure about this one, lots of bit-shifted clock generators?
+	hw/mcu/microchip/samd/asf4/samd21/hpl/pm/hpl_pm.c \ #power management, sleep, reset
+	hw/mcu/microchip/samd/asf4/samd21/hpl/sysctrl/hpl_sysctrl.c #initializesall GCLK generators
 
-INC += \
+INC += \ #include directories to look for files
 	-I$(TOP)/hw/mcu/microchip/samd/asf4/samd21/ \
 	-I$(TOP)/hw/mcu/microchip/samd/asf4/samd21/config \
 	-I$(TOP)/hw/mcu/microchip/samd/asf4/samd21/include \
--- tools/top.mk
+++ tools/top.mk
@@ -1,4 +1,4 @@
-ifneq ($(lastword a b),b)
+ifneq ($(lastword a b),b) #should be the same as $(lastword b), selects and stores the last word in the list
 $(error This Makefile require make 3.81 or newer)
 endif
 
@@ -6,12 +6,12 @@ endif
 # invoked) to the top of the tree. $(lastword $(MAKEFILE_LIST)) returns
 # the name of this makefile relative to where make was invoked.
 
-THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST))
-TOP := $(patsubst %/tools/top.mk,%,$(THIS_MAKEFILE))
+THIS_MAKEFILE := $(lastword $(MAKEFILE_LIST)) #names the makefile after the last word in the list
+TOP := $(patsubst %/tools/top.mk,%,$(THIS_MAKEFILE)) #sets top with a path substitution 
 
-TOP := $(shell realpath $(TOP))
+TOP := $(shell realpath $(TOP)) #sets top with the inherent system path to the home directory
 
-$(info Top directory is $(TOP))
+$(info Top directory is $(TOP)) #variable used to spit out the path
 
-CURRENT_PATH := $(shell realpath --relative-to=$(TOP) `pwd`)
-$(info Path from top is $(CURRENT_PATH))
+CURRENT_PATH := $(shell realpath --relative-to=$(TOP) `pwd`) #gets the current workding directory relative to the top path variable
+$(info Path from top is $(CURRENT_PATH)) #grabs the path from the "top" directory
